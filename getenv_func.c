6badab38123e1abbfe960e91f221039d16ce1454
CaroMusangi1 <kitongacarol8@gmail.com>
2023-10-15 04:10:07 -0700

    getenv_func.c

---
+++ getenv_func.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * _getenv - sources an environment variable name then returns it's value
+ * @variableName: The variable name sourced
+ *
+ * Return: pointer to variable's valu
+ */
+
+char *_getenv(char *variableName)
+{
+	int i;
+	char *path = NULL;
+	size_t var_len = _strlen(variableName);
+
+	for (i = 0; environ[i]; i++)
+	{
+		if (!_strncmp(environ[i], variableName, var_len) && environ[i][var_len] == '=')
+		{
+			path = environ[i] + var_len + 1;
+			return (path);
+		}
+	}
+	return (path);
+}
