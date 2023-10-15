f2135b6a2493a00ac4d638a59034f2fef4183b30
CaroMusangi1 <kitongacarol8@gmail.com>
2023-10-15 03:44:17 -0700

    Prompt.c

---
+++ Prompt.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * display_prompt - prints the prompt sign to standard output
+ * @prompt: prompt sign printed
+ *
+ * Return: (0) in interactive mode and (1) in non interactive mode
+ */
+
+int display_prompt(char *prompt_sign)
+{
+	if (isatty(0))
+		_fputstr(1, prompt_sign);
+
+	return (0);
+}
