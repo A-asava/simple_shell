c6a904c0156b76fb83ba0863002398161116fc0b
A-asava <adikasaid996@gmail.com>
2023-10-17 10:09:01 +0000

    shell_update_version

--- prompt_display.c
+++ prompt_display.c
@@ -2,10 +2,11 @@
 
 /**
  * display_prompt - prints the prompt sign to standard output
- @ prompt_sign: prompt sign printed
+ * @prompt_sign: prompt sign that is printed
  *
  * Return: (0) in interactive mode and (1) in non interactive mode
  */
+
 
 int display_prompt(char *prompt_sign)
 {
